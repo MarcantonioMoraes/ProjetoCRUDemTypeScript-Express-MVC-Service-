af005277b82265262f09f635a9442b55f10e8fdd
Marcantonio <marcantonio.lupercino@gmail.com>
2025-10-02 19:33:16 -0300

    Update TaskController.ts

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -4,10 +4,12 @@ import { TaskView } from "../views/TaskView.js"
 
 export class TaskController {
     private service= new TaskService ();
+
     getAll (req: Request, res: Response): void {
     const tasks = this.service.getAll();
     TaskView.success (res, tasks);
     }
+
     getOne (req: Request, res: Response): void{
     try {
      const id = Number (req.params.id);
@@ -17,7 +19,8 @@ export class TaskController {
     } catch (err: any) {
     TaskView.error (res, err.message, 400);
     }
-}
+    }
+
     create (req: Request, res: Response): void{
     try {
     const { title } = req.body;
@@ -26,25 +29,27 @@ export class TaskController {
     } catch (err: any) {
     TaskView.error (res, err.message, 400);
     }
-}
-update (req: Request, res: Response): void {
-try{
-const id = Number (req.params.id);
-const { title, done } = req.body;
-const task = this.service.update(id, title, done);
-TaskView.success (res, task);
-} catch (err: any) {
-TaskView.error (res, err.message, 400);
-}
-}
-delete (req: Request, res: Response): void {
-try{
-const id = Number (req.params.id);
-const removed = this.service.delete (id);
-if (!removed) return TaskView.error (res, "Tarefa não encontrada", 440);
-TaskView.success (res, { message: "Tarefa removida com sucesso"});
-} catch (err: any) {
-TaskView.error (res, err.message, 400);
-}
-}
+    }
+
+    update (req: Request, res: Response): void {
+    try{
+    const id = Number (req.params.id);
+    const { title, done } = req.body;
+    const task = this.service.update(id, title, done);
+    TaskView.success (res, task);
+    } catch (err: any) {
+    TaskView.error (res, err.message, 400);
+    }
+    }
+    
+    delete (req: Request, res: Response): void {
+    try{
+    const id = Number (req.params.id);
+    const removed = this.service.delete (id);
+    if (!removed) return TaskView.error (res, "Tarefa não encontrada", 440);
+    TaskView.success (res, { message: "Tarefa removida com sucesso"});
+    } catch (err: any) {
+    TaskView.error (res, err.message, 400);
+    }
+    }
 }
